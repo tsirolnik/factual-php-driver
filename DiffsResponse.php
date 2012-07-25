de4a4e3b17b67fcc0d6ca1ad7f458a811795748b
twbell <tyler@factual.com>
2012-07-25 16:30:32 -0700

    Added Diffs

--- DiffsResponse.php
+++ DiffsResponse.php
@@ -1,38 +1,17 @@
 <?php
 
 /**
- * The response from running a FLag query
+ * The response from running a Diffs query
  * @author Tyler
  * @package Factual
  * @license Apache 2.0
  */
-class DiffsResponse extends ArrayIterator {
+class DiffsResponse extends FactualResponse {
 	
 	protected $diffStart = null; //Start time. Unix timestamp in milliseconds
 	protected $diffEnd = null; //Optional end time. Unix timestamp in milliseconds
 	protected $duration = null; //Duration of Diffs window in microseconds
-  	protected $version = array(); 
-  	protected $status = array();
-  	protected $data = array();
-  	protected $json = null;
-  	protected $responseHeaders = array();
-  	protected $responseCode = null;
-  	protected $request = null;	
-	
-  /**
-   * Constructor, parses return values from CURL in factual::request() 
-   * @param array response The JSON response String returned by Factual.
-   */
-  public function __construct($apiResponse) {
-    try {
-    	$this->json = $apiResponse['body'];
-    	$this->parseResponse($apiResponse);
-    } catch (Exception $e) {
-    	//add note about json encoding borking here
-      throw $e ();
-    }
-  }
-
+	protected $stats = array();//summary statistics
 	/**
 	 * Gets Diffs start time in milliseconds
 	 * @param bool human Human Readable (RFC 2822) rather than timestamp
@@ -75,6 +54,11 @@ class DiffsResponse extends ArrayIterator {
 		}
 		return $this->duration;
 	}
+	
+
+	public function getDiffs(){
+		return $this->getArrayCopy();
+	}
 
 	/**
 	 * Parses response from CURL
@@ -82,63 +66,54 @@ class DiffsResponse extends ArrayIterator {
 	 * @return void
 	 */
 	protected function parseResponse($apiResponse){	
+		parent::parseResponse($apiResponse);
+		//assign metadata specific to this call
 		$this->diffStart = $apiResponse['diffsmeta']['start'];
 		$this->diffEnd = $apiResponse['diffsmeta']['end'];
 		$this->duration = $apiResponse['diffsmeta']['end'] - $apiResponse['diffsmeta']['start'];
-		//convert JSON to array
-		$rootJSON = json_decode($apiResponse['body'],true);
-		//assign status value
-    	$this->status = $rootJSON['status'];
-    	//assign version
-    	$this->version = $rootJSON['version'];
-    	//assign indivitual diffs to array
-		foreach ($rootJSON['data'] as $diff){
-			$this->append($diff);
-		}	
-		//assign headers
-		$this->responseHeaders = $apiResponse['headers'];
-		//assign response
-		$this->responseCode = $apiResponse['code'];
-		//assign request string
-		$this->request = $apiResponse['request'];
 	}
 
 	/**
-	 * Test for success (n 200 status return)
+	 * Parses the server response from the API
+	 * @param string json JSON returned from API
+	 * @return void
 	 */
-	 public function success(){
-	 	if ($this->status = 200){
-	 		return true;
-	 	} else {
-	 		return false;
-	 	}
-	 }
-
-  /**
-   * Get the entire JSON response from Factual
-   * @return string 
-   */
-  public function getJson() {
-    return $this->json;
-  }
-
-  /**
-   * Get the status returned by the Factual API server, e.g. "ok".
-   * @return string
-   */
-  public function getStatus() {
-    return $this->status;
-  }
-
-  /**
-   * Get the version returned by the Factual API server, e.g. "3".
-   * @return numeric
-   */
-  public function getVersion() {
-    return $this->version;
-  }
-
-
-
+	protected function parseJSON($json){
+		$allDiffs= explode("\n",trim($json)); //diffs reponses are discreet JSON, linebreak delimited
+		foreach ($allDiffs as $diff){
+			$this[] = json_decode($diff,true);
+		}
+	}
+	
+	
+	public function getStats(){
+		if(empty($this->stats)){
+			$this->generateStats();
+		}
+		return $this->stats;
+	}
+	
+	
+	/**
+	 * Creates summary of all diffs
+	 * @return void
+	 */
+	protected function generateStats(){
+		
+		$this->stats = array(  
+					'insert'=>0,
+					'update'=>0,
+					'delete'=>0,
+					'deprecate'=>0,
+					'total'=>0,
+					'duration'=>null);
+		
+		foreach ($this as $diff){
+			$this->stats[$diff['type']]++;
+		}
+		$this->stats['total'] = count($this);
+		$this->stats['duration'] = $this->getDuration(true);
+	}
+	
 }
 ?>
