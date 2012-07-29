2dc6366949963e9a234f87e0741ffaf733f0c4d0
twbell <tyler@factual.com>
2012-07-29 01:24:27 -0700

    Fixed bug in human-readable duration formatting

--- DiffsResponse.php
+++ DiffsResponse.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /**
  * The response from running a Diffs query
  * @author Tyler
@@ -7,18 +8,18 @@
  * @license Apache 2.0
  */
 class DiffsResponse extends FactualResponse {
-	
+
 	protected $diffStart = null; //Start time. Unix timestamp in milliseconds
 	protected $diffEnd = null; //Optional end time. Unix timestamp in milliseconds
 	protected $duration = null; //Duration of Diffs window in microseconds
-	protected $stats = array();//summary statistics
+	protected $stats = array (); //summary statistics
 	/**
 	 * Gets Diffs start time in milliseconds
 	 * @param bool human Human Readable (RFC 2822) rather than timestamp
 	 * @return mixed diffs start time
 	 */
-	public function getStart($human = false){
-		if ($human){
+	public function getStart($human = false) {
+		if ($human) {
 			return $this->timestampReadable($this->diffStart);
 		}
 		return $this->diffStart;
@@ -29,15 +30,16 @@ class DiffsResponse extends FactualResponse {
 	 * @param bool human Human Readable (RFC 2822) rather than timestamp	 * 
 	 * @return mixed diffs end time
 	 */
-	public function getEnd($human = false){
-		if ($human){
+	public function getEnd($human = false) {
+		if ($human) {
 			return $this->timestampReadable($this->diffEnd);
-		}		
+		}
 		return $this->diffEnd;
 	}
 
-	protected function timestampReadable($timeStamp){
-		return date('r',$timeStamp);
+	protected function timestampReadable($timeStamp) {
+		$timeStamp = $timeStamp / 1000;
+		return date('r', $timeStamp);
 	}
 
 	/**
@@ -45,18 +47,26 @@ class DiffsResponse extends FactualResponse {
 	 * @param bool human Human Readable rather than milliseconds 
 	 * @return mixed duration in milliseconds | H:m:s
 	 */
-	public function getDuration($human = false){
-		if ($human){
-			//$mod = round($this->duration % 1000,3);
-			//$seconds = floor($this->duration / 1000);
+	public function getDuration($human = false) {
+		if ($human) {
+			$mod = round($this->duration % 1000, 3);
+
 			$seconds = $this->duration / 1000;
-			return date("H:i:s",$seconds);
+			$duration = $this->getHumanDuration($seconds);
+			if ($mod) {
+				$duration .= ", " . $mod . " ms";
+			}
+			return $duration;
+
 		}
 		return $this->duration;
 	}
-	
 
-	public function getDiffs(){
+	/**
+	 * Get all Diffs as a array
+	 * @return array
+	 */
+	public function getDiffs() {
 		return $this->getArrayCopy();
 	}
 
@@ -65,8 +75,8 @@ class DiffsResponse extends FactualResponse {
 	 * @param array apiResponse response from curl
 	 * @return void
 	 */
-	protected function parseResponse($apiResponse){	
-		parent::parseResponse($apiResponse);
+	protected function parseResponse($apiResponse) {
+		parent :: parseResponse($apiResponse);
 		//assign metadata specific to this call
 		$this->diffStart = $apiResponse['diffsmeta']['start'];
 		$this->diffEnd = $apiResponse['diffsmeta']['end'];
@@ -78,42 +88,81 @@ class DiffsResponse extends FactualResponse {
 	 * @param string json JSON returned from API
 	 * @return void
 	 */
-	protected function parseJSON($json){
-		$allDiffs= explode("\n",trim($json)); //diffs reponses are discreet JSON, linebreak delimited
-		foreach ($allDiffs as $diff){
-			$this[] = json_decode($diff,true);
+	protected function parseJSON($json) {
+		$allDiffs = explode("\n", trim($json)); //diffs reponses are discreet JSON, linebreak delimited
+		foreach ($allDiffs as $diff) {
+			$this[] = json_decode($diff, true);
 		}
 	}
-	
-	
-	public function getStats(){
-		if(empty($this->stats)){
+
+	public function getStats() {
+		if (empty ($this->stats)) {
 			$this->generateStats();
 		}
 		return $this->stats;
 	}
-	
-	
+
 	/**
 	 * Creates summary of all diffs
 	 * @return void
 	 */
-	protected function generateStats(){
-		
-		$this->stats = array(  
-					'insert'=>0,
-					'update'=>0,
-					'delete'=>0,
-					'deprecate'=>0,
-					'total'=>0,
-					'duration'=>null);
-		
-		foreach ($this as $diff){
+	protected function generateStats() {
+
+		$this->stats = array (
+			'insert' => 0,
+			'update' => 0,
+			'delete' => 0,
+			'deprecate' => 0,
+			'total' => 0,
+			'duration' => null
+		);
+
+		foreach ($this as $diff) {
 			$this->stats[$diff['type']]++;
 		}
 		$this->stats['total'] = count($this);
 		$this->stats['duration'] = $this->getDuration(true);
+		$this->stats['start'] = $this->getStart(true);
+		$this->stats['end'] = $this->getEnd(true);
 	}
-	
+
+	/**
+	 * Convert window duration to human readable format
+	 * @internal From http://aidanlister.com/2004/04/making-time-periods-readable/
+	 */
+	protected function getHumanDuration($seconds) {
+		// Define time periods
+		$periods = array (
+			'days' => 86400,
+			'hours' => 3600,
+			'minutes' => 60,
+			'seconds' => 1
+		);
+
+		// Break into periods
+		$seconds = (float) $seconds;
+		$segments = array ();
+		foreach ($periods as $period => $value) {
+			$count = floor($seconds / $value);
+			if ($count == 0) {
+				continue;
+			}
+			$segments[strtolower($period)] = $count;
+			$seconds = $seconds % $value;
+		}
+		// Build the string
+		$string = array ();
+		foreach ($segments as $key => $value) {
+			$segment_name = substr($key, 0, -1);
+			$segment = $value . ' ' . $segment_name;
+			if ($value != 1) {
+				$segment .= 's';
+			}
+			$string[] = $segment;
+		}
+
+		return implode(', ', $string);
+	}
+
 }
 ?>
