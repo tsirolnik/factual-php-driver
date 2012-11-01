96a42a4bbc375260453e85a7dd334cf47d86c826
twbell <tyler@factual.com>
2012-10-31 17:01:04 -0700

    script now takes key and secret as parameters on cmdln

--- test.php
+++ test.php
@@ -1,25 +1,29 @@
 <?php
 
 /*
- * Run this script in CLI after setting your key and secrect below. See https://github.com/Factual/factual-php-driver#testing
+ * Run this script in CLI using key as parameter 1 and secret as parameter 2, e.g.
+ * 			php test.php yourFactualKey yourFactualSecret [logFile]
+ * See https://github.com/Factual/factual-php-driver/wiki/Getting-Started
  */
 
-//Add your key and secret here
-$key = "";
-$secret = "";
+if (isset($argv[1])){
+	$key = $argv[1];
+	$secret = $argv[2];
+	if (isset($argv[3])){$logFile = $argv[3];} else {$logFile="";}
+} else {
+	echo "Usage: php test.php key secret [log file]\n";
+	echo "Add your key and secret as parameters to this command line script. See https://github.com/Factual/factual-php-driver/wiki/Getting-Started for more info\n";
+	exit;
+}
 
-//Add filename for to log results to file, if required (echoes to screen by default)
-$logFile = "";
+if (empty($key) || empty($secret)){
+	echo "Your Facual Key and Secret are required parameters. See https://github.com/Factual/factual-php-driver/wiki/Getting-Started for more info\n";
+	exit;
+}
+
 
 //Set error level -- best not to change this.
 error_reporting (E_ERROR);
-
-//check that key/secret are populated
-if (!$key || !$secret){
-	echo "Add your key and secret to test.php before running this script\n";
-	echo "See https://github.com/Factual/factual-php-driver/wiki/Getting-Started for more info\n";
-	exit;
-}
 
 require_once('FactualTest.php');
 	
