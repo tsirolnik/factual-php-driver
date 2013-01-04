4d8eae09c96f9ff47977ead5b61828732b6ca879
twbell <tyler@factual.com>
2013-01-03 20:51:19 -0800

    Added strict mode

--- FactualSubmittor.php
+++ FactualSubmittor.php
@@ -1,45 +1,63 @@
 <?php
 /**
- * Factual Submittor vars collector
+ * Factual Submittor vars collector for adding, updating, and clearing values
  * @author Tyler
  * @package Factual
  * @license Apache 2.0
  */
  require_once("FactualPost.php");
  class FactualSubmittor extends FactualPost{
-	//Required Params
-	public $values = array();   //values to be added/updated
+	//Required Params	(uses API parameter names)
+	protected $values = array();	//values to be submitted
+	protected $strict = false;		//strict mode (accept submissions with only valid attributes)
+	protected $clear_blanks = false; //clear all empty values
 		
-	/**
-	 * Sets Factual ID for entity updates
-	 * @return string the Factual ID
-	 * 
-	 */
-	public function setFactualID($var){
-		parent::setFactualID($var);
- 		$this->setValue("factual_id",$var); //also add this as a value to the post array
- 		return $this->factualID;
- 	} 		
-	
-	/**
-	 * Gets the object vars required for POST
-	 * @return array
-	 */
- 	protected function getPostVars(){
- 		return array_merge(parent::getPostVars(),array("values"));
+	 /**
+	  * Turns strict mode (reject submission of contains invalid attribute names) on/off
+	  * @param bool safe
+	  */		
+	public function strictMode($strict=true){
+		$this->strict = $strict;
+		return $this->strict;
+	} 		
+
+	 /**
+	  * Tells Factual Submit API to interpret empty string values as 'cleared'
+	  * Use with care
+	  * @param bool clear
+	  */		
+	public function clearBlanks($blanks=true){
+		$this->clear_blanks = $blanks;
+		return $this->clear_blanks;
+	} 		 		
+	 		 	 		
+ 	/**
+ 	 * Overrides parent class
+ 	 * @return array
+ 	 */	
+ 	public function getPostVars(){
+ 		$postVars = array("values");
+ 		if ($this->strict){
+ 			$postVars = "strict";	
+ 		}
+ 		if ($this->clear_blanks){
+ 			$postVars[] = "clear_blanks";	
+ 		} 		
+ 		return array_merge(parent::getPostVars(),$postVars);
  	}
- 		
+ 	
  	/**
  	 * Checks whether required params are included before writing
  	 * @return bool
  	 */
  	public function isValid(){
+ 		//all submits require a table name, a user token, and values
  		if (empty($this->tableName)| empty($this->user)|empty($this->values)){
  			return false;
  		} else {
  			return true;
  		}	
- 	}
+ 	} 	
  	
  	/**
  	 * Adds key/value pairs to object
@@ -54,18 +72,17 @@
  	
  	/**
  	 * Adds array of key/value pairs to object
- 	 * @param array data key/value pairs
- 	 * @param mixed value Value to add/edit/update
+ 	 * @param array data key/value pairs to add/update
  	 * @return array set values
  	 */
  	public function setValues($data){
  		if (!is_array($data)){
- 			throw new exception ("Parameter must be assoc. array");
+ 			throw new exception (__METHOD__." Parameter must be assoc. array: key = attribute name, value = attribute value");
  		}
  		foreach ($data as $key => $value){
  			$this->values[$key] = $value;	
  		}
  		return $this->values;
- 	} 	
+ 	}
  }
 ?>
